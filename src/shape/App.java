269abca84a38b2cff3ff71559dfb2be25d93d4a4
noah-lvc <lavocatnoah@gmail.com>
2026-02-23 11:09:34 +0100

    ajout de la documentation au fichier App.java

--- src/shape/App.java
+++ src/shape/App.java
@@ -2,17 +2,40 @@ package shape;
 
 import java.util.ArrayList;
 
+/**
+ * Classe principale de l'application Pixel Tracer.
+ * Elle gère le cycle de vie de l'application, maintient la liste des zones de dessin
+ * et orchestre l'exécution des commandes utilisateur.
+ */
 public class App {
+
+    /** Liste des zones de dessin (Area) créées dans l'application. */
     private ArrayList<Area> areas;
+
+    /** La zone de dessin actuellement sélectionnée. */
     public Area currentArea;
+
+    /** Le calque (Layer) actuellement sélectionné. */
     public Layer currentLayer;
+
+    /** La forme (Shape) actuellement sélectionnée. */
     public Shape currentShape;
+
+    /** La commande en cours de traitement. */
     private Command command;
 
+    /**
+     * Constructeur de l'application.
+     * Initialise la liste des zones de dessin.
+     */
     public App() {
         areas = new ArrayList<>();
     }
 
+    /**
+     * Initialise l'application, crée la zone par défaut et lance la boucle 
+     * principale d'écoute des commandes utilisateur.
+     */
     public void init() {
         System.out.println("Pixel Tracer App");
         Area area = new Area(40, 40, IdCounter.getInstance().getNextId(), "default");
@@ -30,14 +53,26 @@ public class App {
         }
     }
 
+    /**
+     * Ajoute une nouvelle zone de dessin à la liste de l'application.
+     * * @param area La zone de dessin à ajouter.
+     */
     public void addAreaToList(Area area) {
         areas.add(area);
     }
 
+    /**
+     * Récupère la liste de toutes les zones de dessin.
+     * * @return Une ArrayList contenant les instances d'Area.
+     */
     public ArrayList<Area> getAreas() {
         return areas;
     }
 
+    /**
+     * Point d'entrée principal du programme.
+     * * @param args Arguments de la ligne de commande (non utilisés).
+     */
     public static void main(String[] args) {
         App app = new App();
         app.init();

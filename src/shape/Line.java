2eedd812f1ed72d9a4280ff3b8f7b1aff3c2a46a
noah-lvc <lavocatnoah@gmail.com>
2026-02-23 11:31:21 +0100

    ajout de la documentation au fichier Line.java

--- src/shape/Line.java
+++ src/shape/Line.java
@@ -1,90 +1,82 @@
 package shape;
 
 /**
- * Class Line
+ * Classe Line
  *
- * Représente une ligne composée d'une suite de points.
- * Hérite de Shape.
+ * Représente une ligne droite (segment) définie par deux points.
+ * Hérite de la classe Shape.
  *
  * Propriétés spécifiques :
- *  - points : liste de Point composant la ligne
+ * - p1 : point de départ du segment
+ * - p2 : point d'arrivée du segment
  *
- * Implémente draw() pour dessiner la ligne (affiche nombre de points pour simulation).
+ * Implémente draw() pour dessiner la ligne sur une zone de pixels.
  */
 public class Line extends Shape {
 
-  //
-  // Fields
-  //
+/** Premier point (origine) de la ligne. */
+private Point p1;
 
-  private Point p1;
-  private Point p2;
+/** Deuxième point (destination) de la ligne. */
+private Point p2;
   
-  //
-  // Constructors
-  //
+  /**
+   * Constructeur d'une ligne à partir de deux objets Point.
+   * @param p1 Le point de départ.
+   * @param p2 Le point d'arrivée.
+   */
   public Line(Point p1, Point p2) {
     super(IdCounter.getInstance().getNextId(), ShapeType.LINE);
     this.p1 = p1;
     this.p2 = p2;
   }
-  
-  //
-  // Methods
-  //
-
-
-  //
-  // Accessor methods
-  //
-
+  /**
+   * Demande à la zone de dessin de tracer le segment entre p1 et p2.
+   * @param area La zone de dessin (Area) où la ligne doit être rendue.
+   */
     public void draw(Area area) {
         area.drawLine(p1, p2);
     }
 
   /**
-   * Set the value of p1
-   * @param newVar the new value of p1
+   * Définit le premier point de la ligne.
+   * @param newVar Le nouveau point p1.
    */
   public void setP1 (Point newVar) {
     p1 = newVar;
   }
 
   /**
-   * Get the value of p1
-   * @return the value of p1
+   * Récupère le premier point de la ligne.
+   * @return Le point p1.
    */
   public Point getP1 () {
     return p1;
   }
 
   /**
-   * Set the value of p2
-   * @param newVar the new value of p2
+   * Définit le second point de la ligne.
+   * @param newVar Le nouveau point p2.
    */
   public void setP2 (Point newVar) {
     p2 = newVar;
   }
 
   /**
-   * Get the value of p2
-   * @return the value of p2
+   * Récupère le second point de la ligne.
+   * @return Le point p2.
    */
   public Point getP2 () {
     return p2;
   }
 
-  //
-  // Other methods
-  //
-
   /**
-   * @return
+   * Retourne une représentation textuelle des points de la ligne.
+   * @return Une chaîne de caractères décrivant p1 et p2.
    */
   public String print()
   {
     return "points :\np1 : " + p1.print() + "\np2 : " + p2.print();
   }
-
-
 }
+

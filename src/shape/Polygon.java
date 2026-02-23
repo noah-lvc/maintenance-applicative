2783149b28c25908449287ce2295b89140080136
noah-lvc <lavocatnoah@gmail.com>
2026-02-23 11:35:57 +0100

    ajout de la documentation au fichier Polygon.java

--- src/shape/Polygon.java
+++ src/shape/Polygon.java
@@ -3,30 +3,41 @@ package shape;
 import java.util.ArrayList;
 
 /**
- * Class Polygon
+ * Classe Polygon
+ * * Représente une forme géométrique fermée composée de plusieurs sommets (points).
+ * Hérite de la classe Shape.
  */
 public class Polygon extends Shape {
 
-    //
-    // Fields
-    //
-
+    /** Nombre de sommets du polygone. */
     private int n;
+
+    /** Liste des points (sommets) constituant le polygone. */
     private ArrayList<Point> points;
 
-    //
-    // Constructors
-    //
+    /**
+     * Construit un polygone à partir d'une liste de points existante.
+     * @param pointsList La liste des points à utiliser comme sommets.
+     */
     public Polygon(ArrayList<Point> pointsList) {
         super(IdCounter.getInstance().getNextId(), ShapeType.POLYGON);
         points = pointsList;
     }
 
+    /**
+     * Construit un polygone vide avec une capacité initiale de n points.
+     * @param n Capacité initiale de la liste de points.
+     */
     public Polygon(int n) {
         super(IdCounter.getInstance().getNextId(), ShapeType.POLYGON);
         points = new ArrayList<Point>(n);
     }
 
+    /**
+     * Construit un polygone à partir d'un nombre de points et d'un tableau de coordonnées.
+     * @param n Nombre de points.
+     * @param xandy Tableau contenant les coordonnées x et y à la suite (x1, y1, x2, y2...).
+     */
     public Polygon(int n, int[] xandy) {
         super(IdCounter.getInstance().getNextId(), ShapeType.POLYGON);
         points = new ArrayList<Point>(n);
@@ -43,59 +54,57 @@ public class Polygon extends Shape {
 
 
 
-    //
-    // Methods
-    //
+    /**
+     * Ajoute un nouveau sommet au polygone.
+     * @param p Le point à ajouter.
+     */
     public void addPoint(Point p) {
         points.add(p);
     }
 
-    //
-    // Accessor methods
-    //
-
+    /**
+     * Demande à la zone de dessin de tracer le polygone en reliant ses sommets.
+     * @param area La zone de dessin (Area) où le polygone doit être rendu.
+     */
     public void draw(Area area) {
         area.drawPolygon(points);
     }
 
     /**
-     * Set the value of n
-     * @param newVar the new value of n
+     * Définit le nombre de sommets n.
+     * @param newVar La nouvelle valeur de n.
      */
     public void setN (int newVar) {
         n = newVar;
     }
 
     /**
-     * Get the value of n
-     * @return the value of n
+     * Récupère le nombre de sommets n.
+     * @return La valeur de n.
      */
     public int getN () {
         return n;
     }
 
     /**
-     * Set the value of points
-     * @param newVar the new value of points
+     * Définit la liste des points du polygone.
+     * @param newVar La nouvelle liste de points.
      */
     public void setPoints (ArrayList<Point> newVar) {
         points = newVar;
     }
 
     /**
-     * Get the value of points
-     * @return the value of points
+     * Récupère la liste des points du polygone.
+     * @return La liste des sommets (ArrayList).
      */
     public ArrayList<Point> getPoints () {
         return points;
     }
 
-    //
-    // Other methods
-    //
-
     /**
-     * @return
+     * Retourne une représentation textuelle du polygone et de ses points.
+     * @return Une chaîne de caractères listant tous les sommets et la valeur n.
      */
     public String print()
     {

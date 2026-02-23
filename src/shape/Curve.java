f185de1f9ac41eff8839c7c6af698538b751fd72
noah-lvc <lavocatnoah@gmail.com>
2026-02-23 11:24:35 +0100

    ajout de la documentation au fichier Curve.java

--- src/shape/Curve.java
+++ src/shape/Curve.java
@@ -1,30 +1,32 @@
 package shape;
 
 /**
- * Class Curve
+ * Classe Curve
  *
- * Représente une courbe composée d'une suite de points.
+ * Représente une courbe de Bézier cubique définie par quatre points de contrôle.
  * Hérite de Shape.
  *
  * Propriétés spécifiques :
- *  - points : liste de Point composant la courbe
- *
- * Implémente draw() pour dessiner la courbe (affiche nombre de points pour simulation).
+ * - p1, p2, p3, p4 : les quatre points de contrôle de la courbe.
  */
 public class Curve extends Shape {
 
-  //
-  // Fields
-  //
+ /** Premier point d'ancrage (départ). */
+ private Point p1;
+ /** Premier point de contrôle. */
+ private Point p2;
+ /** Deuxième point de contrôle. */
+ private Point p3;
+ /** Deuxième point d'ancrage (arrivée). */
+ private Point p4;
 
-  private Point p1;
-  private Point p2;
-  private Point p3;
-  private Point p4;
-  
-  //
-  // Constructors
-  //
+ /**
+   * Constructeur de la courbe avec quatre points de contrôle.
+   * @param p1 Point de départ.
+   * @param p2 Premier point de contrôle.
+   * @param p3 Deuxième point de contrôle.
+   * @param p4 Point d'arrivée.
+   */
   public Curve(Point p1, Point p2, Point p3, Point p4) {
     super(IdCounter.getInstance().getNextId(), ShapeType.CURVE);
     this.p1 = p1;
@@ -33,88 +35,81 @@ public class Curve extends Shape {
     this.p4 = p4;
   }
   
-  //
-  // Methods
-  //
-
-  //
-  // Accessor methods
-  //
-
-    public void draw(Area area) {
+  /**
+   * Demande à la zone de dessin de tracer la courbe de Bézier.
+   * @param area La zone de dessin où la courbe sera rendue.
+   */
+  public void draw(Area area) {
         area.drawCurve(p1, p2, p3, p4);
     }
 
   /**
-   * Set the value of p1
-   * @param newVar the new value of p1
+   * Définit la valeur de p1.
+   * @param newVar La nouvelle valeur pour p1.
    */
   public void setP1 (Point newVar) {
     p1 = newVar;
   }
 
   /**
-   * Get the value of p1
-   * @return the value of p1
+   * Récupère la valeur de p1.
+   * @return La valeur de p1.
    */
   public Point getP1 () {
     return p1;
   }
 
   /**
-   * Set the value of p2
-   * @param newVar the new value of p2
+   * Définit la valeur de p2.
+   * @param newVar La nouvelle valeur pour p2.
    */
   public void setP2 (Point newVar) {
     p2 = newVar;
   }
 
   /**
-   * Get the value of p2
-   * @return the value of p2
+   * Récupère la valeur de p2.
+   * @return La valeur de p2.
    */
   public Point getP2 () {
     return p2;
   }
 
   /**
-   * Set the value of p3
-   * @param newVar the new value of p3
+   * Définit la valeur de p3.
+   * @param newVar La nouvelle valeur pour p3.
    */
   public void setP3 (Point newVar) {
     p3 = newVar;
   }
 
   /**
-   * Get the value of p3
-   * @return the value of p3
+   * Récupère la valeur de p3.
+   * @return La valeur de p3.
    */
   public Point getP3 () {
     return p3;
   }
 
   /**
-   * Set the value of p4
-   * @param newVar the new value of p4
+   * Définit la valeur de p4.
+   * @param newVar La nouvelle valeur pour p4.
    */
   public void setP4 (Point newVar) {
     p4 = newVar;
   }
 
   /**
-   * Get the value of p4
-   * @return the value of p4
+   * Récupère la valeur de p4.
+   * @return La valeur de p4.
    */
   public Point getP4 () {
     return p4;
   }
 
-  //
-  // Other methods
-  //
-
   /**
-   * @return
+   * Retourne une représentation textuelle des points de la courbe.
+   * @return Une chaîne de caractères listant les coordonnées des points p1 à p4.
    */
   public String print()
   {

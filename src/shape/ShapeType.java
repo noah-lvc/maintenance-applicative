28225353ec9d48d84be6a3c6956c6f04f511cbd9
noah-lvc <lavocatnoah@gmail.com>
2026-02-23 11:44:59 +0100

    ajout de la documentation au fichier Square.java

--- src/shape/ShapeType.java
+++ src/shape/ShapeType.java
@@ -1,5 +1,30 @@
 package shape;
 
+/**
+ * Énumération ShapeType
+ * * Définit la liste exhaustive des types de formes géométriques 
+ * gérées par l'application Pixel Tracer.
+ * Utilisée pour l'identification et le filtrage des objets graphiques.
+ */
 public enum ShapeType {
-    POINT, LINE, SQUARE, RECTANGLE, CIRCLE, POLYGON, CURVE
+    /** Représente un point unique (x, y). */
+    POINT, 
+    
+    /** Représente un segment de droite entre deux points. */
+    LINE, 
+    
+    /** Représente un quadrilatère régulier (quatre côtés égaux). */
+    SQUARE, 
+    
+    /** Représente un quadrilatère dont les angles sont droits. */
+    RECTANGLE, 
+    
+    /** Représente une forme circulaire définie par un centre et un rayon. */
+    CIRCLE, 
+    
+    /** Représente une forme fermée composée de plusieurs segments. */
+    POLYGON, 
+    
+    /** Représente une courbe de Bézier définie par des points de contrôle. */
+    CURVE
 }
--- src/shape/Square.java
+++ src/shape/Square.java
@@ -1,86 +1,78 @@
 package shape;
 
-
 /**
  * Class Square
  *
- * Représente un carré (spécialisation d'un rectangle).
- * Hérite de Shape.
+ * Représente un carré défini par un point d'origine et la longueur de son côté.
+ * Hérite de la classe Shape.
  *
  * Propriétés spécifiques :
- *  - length : longueur du côté
+ * - p1 : le point correspondant au coin supérieur gauche du carré
+ * - length : la longueur du côté du carré
  *
- * Implémente draw() pour dessiner le carré (affiche longueur du côté).
+ * Implémente draw() pour effectuer le rendu du carré sur une zone de pixels.
  */
 public class Square extends Shape {
 
-  //
-  // Fields
-  //
-
+  /** Le coin supérieur gauche du carré. */
   private Point p1;
+  /** La longueur des quatre côtés du carré. */
   private int length;
   
-  //
-  // Constructors
-  //
+  /**
+   * Constructeur du carré.
+   * @param p1 Le coin supérieur gauche du carré.
+   * @param length La longueur du côté.
+   */
   public Square(Point p1, int length) {
     super(IdCounter.getInstance().getNextId(), ShapeType.SQUARE);
     this.p1 = p1;
     this.length = length;
   }
 
-  
-  //
-  // Methods
-  //
-
-
-  //
-  // Accessor methods
-  //
+ /**
+   * Demande à la zone de dessin de tracer les contours de ce carré.
+   * @param area La zone de dessin (Area) où le carré doit être rendu.
+   */
   public void draw(Area area) {
       area.drawSquare(p1, length);
   }
 
   /**
-   * Set the value of p1
-   * @param newVar the new value of p1
+   * Définit le point d'origine du carré.
+   * @param newVar Le nouveau point pour le coin supérieur gauche.
    */
   public void setP1 (Point newVar) {
     p1 = newVar;
   }
 
   /**
-   * Get the value of p1
-   * @return the value of p1
+   * Récupère le point d'origine du carré.
+   * @return Le point p1.
    */
   public Point getP1 () {
     return p1;
   }
 
   /**
-   * Set the value of length
-   * @param newVar the new value of length
+   * Définit la longueur du côté du carré.
+   * @param newVar La nouvelle longueur du côté.
    */
   public void setLength (int newVar) {
     length = newVar;
   }
 
   /**
-   * Get the value of length
-   * @return the value of length
+   * Récupère la longueur du côté du carré.
+   * @return La longueur actuelle du côté.
    */
   public int getLength () {
     return length;
   }
 
-  //
-  // Other methods
-  //
-
   /**
-   * @return
+   * Retourne une représentation textuelle de la position et de la taille du carré.
+   * @return Une chaîne de caractères décrivant le point p1 et la longueur du côté.
    */
   public String print()
   {
